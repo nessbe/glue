9e98cb606a68528e6e1eaf44c8698bf64cf941ca
Nessbe <felix.chaupite@gmail.com>
2025-11-01 18:53:41 +0100

    Core: Implemented core header and source files

---
+++ include/glue/glue.h
@@ -0,0 +1,18 @@
+// File:       glue.h
+// Project:    glue
+// Repository: https://github.com/nessbe/glue
+//
+// Copyright (c) 2025 nessbe
+// This file is part of the glue project and is licensed
+// under the terms specified in the LICENSE file located at the
+// root of this repository.
+//
+// Unless required by applicable law or agreed to in writing,
+// the software is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the LICENSE file for details.
+
+#ifndef GLUE_H
+#define GLUE_H
+
+#endif
